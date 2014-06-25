3e14c6f6896b4871ba125dc055ffac104c4ec940
bmc00019 <bobby.cummings@vice.com>
2014-06-25 17:05:02 -0400

    got it

--- analyticstest/index.php
+++ analyticstest/index.php
@@ -24,12 +24,60 @@ $client->setAssertionCredentials($creds);
 $service = new Google_Service_Analytics($client);
 
 $test = $service->management_webproperties->listManagementWebproperties('~all');
+$accounts = $service->management_accounts->listManagementAccounts();
 
-$analyticsService = new Google_Service_Analytics($client);
-$ga = $service->ga;
+$tablet_mobile = 'gaid::-11';
+
+$segments = $service->management_segments->listManagementSegments();
+$goals = $service->management_goals->listManagementGoals("~all", "~all", "~all");
+$profiles = $service->management_profiles->listManagementProfiles("~all", "~all"); 
+
+// data variables
+// $ids = 'ga:599058';
+$ids = 'ga:73730733';
+$start_date = '2014-06-01';
+$end_date = '2014-06-24';
+// $metrics = "ga:sessions,ga:visits,ga:pageviews,ga:users";
+$metrics = 'ga:sessions,ga:visits,ga:pageviews';
+$dimensions = "ga:browser";
+$optParams = array(
+	'segment' => $tablet_mobile,
+	);
+$mobile_tablet_sessions = $service->data_ga->get($ids,$start_date,$end_date,$metrics,$optParams);
+
+$c_start_date = '2013-01-01';
+$c_end_date = '2013-12-31';
+$c_dimensions = 'ga:country';
+$c_metrics = 'ga:sessions';
+$c_optParams = array(
+	'sort' => '-ga:sessions',
+	'dimensions' => $c_dimensions,
+	);
+$sessions_by_country = $service->data_ga->get($ids,$c_start_date,$c_end_date,$c_metrics,$c_optParams);
 
 
 
+// $mts = array_pop($mobile_tablet_sessions);
 
 echo "<pre>";
-var_dump($ga);
+
+echo "Mobile and Tablet Segment for June 1 - June 24: ";
+echo "<br/>";
+$mts = $mobile_tablet_sessions->rows;
+$mts = array_pop($mts);
+echo "Users: " . $mts[0] . "<br/>";
+echo "Sessions: " . $mts[1] . "<br/>";
+echo "Page Views: " . $mts[2] . "<br/>";
+
+echo "<br/><br/>";
+echo "Total Visits in 2013 by Country (descending): ";
+echo "<br/>";
+// var_dump($sessions_by_country);
+$sbs = $sessions_by_country->rows;
+var_dump($sbs);
+// var_dump($sbs);
+// foreach ($sbs as $key => $value) {
+// 	$bla = array_pop($sbs);
+// 	var_dump($bla);
+// }
+?>
